51a3007d7b08e825e425e23339428a28484eba7c
RihardXXX <yusupa.akaeva@yandex.ru>
2022-04-27 19:12:09 +0300

    add styles for forms

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -1,5 +1,6 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Input from "./Input";
+import colors from "../../constants/colors";
 
 const Form = () => {
 
@@ -8,28 +9,36 @@ const Form = () => {
     };
 
     return (
-        <View>
-            <Input 
-                label={'amount'}
-                textInputConfig={{
-                    maxLength: 6,
-                    keyboardType: 'number-pad',
-                    onChangeText: () => amountHandler() 
-                }}
-            />
-            <Input 
-                label={'date'}
-                textInputConfig={{
-                    maxLength: 10,
-                    keyboardType: 'default',
-                    placeholder: 'YYYY-MM-DD',
-                    onChangeText: () => {}
-                }}
-            />
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Expenses 
+            </Text>
+            <View style={styles.upSection}>
+                <Input 
+                    style={styles.rowInput}
+                    label={'amount'}
+                    textInputConfig={{
+                        maxLength: 6,
+                        keyboardType: 'number-pad',
+                        onChangeText: () => amountHandler() 
+                    }}
+                />
+                <Input 
+                    style={styles.rowInput}
+                    label={'date'}
+                    textInputConfig={{
+                        maxLength: 10,
+                        keyboardType: 'default',
+                        placeholder: 'YYYY-MM-DD',
+                        onChangeText: () => {}
+                    }}
+                />
+            </View>
             <Input 
                 label={'description'}
                 textInputConfig={{
-                    multiline: true
+                    multiline: true,
+                    maxLength: 300,
                 }}
             />
         </View>
@@ -37,3 +46,22 @@ const Form = () => {
 };
 
 export default Form;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 50,
+    },
+    upSection: {
+        flexDirection: "row",
+        justifyContent: 'space-between'
+    },  
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: colors.primary800,
+        textAlign: 'center'
+    },
+    rowInput: {
+        flex: 1,
+    },
+})
--- components/ui/Input.js
+++ components/ui/Input.js
@@ -1,16 +1,49 @@
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import colors from "../../constants/colors";
 
-const Input = ({label, textInputConfig}) => {
+const Input = ({label, textInputConfig, style}) => {
+
+    const inputStyles = [styles.input];
+
+    if (textInputConfig && textInputConfig.multiline) {
+        inputStyles.push(styles.multiline);
+    }
+
     return (
-        <View>
-            <Text>
+        <View style={[styles.container, style]}>
+            <Text style={styles.label}>
                 { label }
             </Text>
-            <TextInput {...textInputConfig} />
+            <TextInput 
+                {...textInputConfig} 
+                style={inputStyles}
+            />
         </View>
     );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 8,
+        marginHorizontal: 15,
+    },
+    label: {
+        fontSize: 14,
+        color: colors.primary700,
+        marginBottom: 5
+    },
+    input: {
+        fontSize: 18,
+        backgroundColor: 'white',
+        paddingVertical: 5,
+        paddingHorizontal: 8,
+        borderRadius: 3,
+        color: colors.primary700,
+    },
+    multiline: {
+        minHeight: 100,
+        textAlignVertical: 'top'
+    }
+});
